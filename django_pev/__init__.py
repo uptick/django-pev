3a5444d7a92e0833b8ff504d9f065205f0122708
william chu <william.chu@uptickhq.com>
2022-12-13 13:29:30 +1100

    feat: Auto add the version number and bump to 0.1.2

--- django_pev/__init__.py
+++ django_pev/__init__.py
@@ -1,1 +1,9 @@
+# mypy: ignore-errors
 from .utils import explain  # NOQA
+
+try:
+    import importlib.metadata as importlib_metadata  # type: ignore[import]
+except ModuleNotFoundError:
+    import importlib_metadata  # type: ignore[import]
+
+__version__ = importlib_metadata.version(__name__)
