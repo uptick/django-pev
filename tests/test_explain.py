b933163100aeca4a53168093738addbabf8e1d52
Raphael Gaschignard <raphael.gaschignard@uptickhq.com>
2026-02-26 10:23:03 +1000

    feat: add support for Django 5.2

--- tests/test_explain.py
+++ tests/test_explain.py
@@ -1,5 +1,6 @@
 from django.db import connection
 from django.test import TestCase
+from time import sleep
 
 from django_pev import explain
 from example.school.models import Student, Subject, Teacher
@@ -22,11 +23,17 @@ class TestExplain(TestCase):
         with explain(db_alias="default") as e:
             list(Student.objects.filter(name="lol"))
             Student.objects.create(name="1")
-            list(Student.objects.raw("select school_student.*, pg_sleep(0.01) from school_student"))
+            list(
+                Student.objects.raw(
+                    "select school_student.*, pg_sleep(0.01) from school_student"
+                )
+            )
 
         assert e.n_queries == 3, "We should have captured 3 queries"
 
-        assert "PG_SLEEP" in e.slowest.sql, "The slowest query should be the one with pg_sleep"
+        assert (
+            "PG_SLEEP" in e.slowest.sql
+        ), "The slowest query should be the one with pg_sleep"
 
     def test_upload_plan_to_dalibo(self):
         # We can upload results to dalibo
@@ -45,7 +52,8 @@ class TestExplain(TestCase):
             list(Student.objects.filter(name="1"))
 
         pev_result = e.slowest.visualize_in_browser()
-
+        # give dalibo a fighting chance to display the result in the browser
+        sleep(5)
         pev_result.delete()
 
     def test_optimization_prompt(self):
